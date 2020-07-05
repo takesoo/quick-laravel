8a29d5957882074277ab9bc91a3a5ef18acdb1a4
hotaka kubota <hotaka.kubota@ai-e.co.jp>
2020-07-05 14:16:25 +0900

    create hello world

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+  public function index()
+  {
+    return 'こんにちは、世界！';
+  }
+
+  public function view()
+  {
+    $data = [
+      'msg' => 'こんばんは、世界！'
+    ];
+
+    return view('hello.view', $data);
+  }
+}
